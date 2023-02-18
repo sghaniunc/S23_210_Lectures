dafefcc7f528547669c1699ceeddc95fd69e6f59
sghaniunc <sghani@cs.unc.edu>
2023-02-18 16:27:05 -0500

    Lectur 10 - Linked List Implementation with Interface

---
+++ src/lec10/Test.java
@@ -0,0 +1,35 @@
+package lec10;
+
+// Demo of a Test implementation of LListInt
+
+public class Test implements LListInt{
+    @Override
+    public Node getHead() {
+        return null;
+    }
+
+    @Override
+    public Node getTail() {
+        return null;
+    }
+
+    @Override
+    public Node insertNode(int value) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteNode(int value) {
+        return false;
+    }
+
+    @Override
+    public int search(Node node, int n, int i) {
+        return 0;
+    }
+
+    @Override
+    public Node search(Node node, int n) {
+        return null;
+    }
+}
